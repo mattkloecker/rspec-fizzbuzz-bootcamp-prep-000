1faa784c9f019ed23ffcbb4611756c4839705f55
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-28 19:19:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0 #if the number is divisble by 3
+  if int % 5 && % 3 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0 #if the number is divisble by 3
     "Fizz"
   elsif int % 5 == 0
-  "Buzz"
-  elsif int % 5 && % 3 == 0
-  "FizzBuzz"
+    "Buzz"
+
  end
 
 
