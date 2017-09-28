37e33d2030ddc0630b0e31ecf868fcd10daebba8
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-28 19:11:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,5 @@
 def fizzbuzz(int)
   if int % 3 == 0 #if the number is divisble by 3
     "Fizz"
-end
-
-  if int % 5 == 0 #if the number is divisible by 5
-    "Buzz"
-
-
-  if (int % 5 == 0 && int % 3 == 0) #if the number is divisble by 3 and 5
-    "FizzBuzz"
-
-
-  if !(int % 5 == 0 || int % 3 == 0) #if the number is not divisble by 3 or 5
-    nil
   end
 end
